7030dba53c04929b709f1415cafbd50928fa1b7b
Numerlor <25886452+Numerlor@users.noreply.github.com>
2021-02-07 16:12:55 +0100

    Add button context menu

--- game_of_life/window.py
+++ game_of_life/window.py
@@ -3,6 +3,37 @@ import pyglet
 from . import CELL_SIZE, HEIGHT, WIDTH, GridManager, SIMULATION_TICK
 
 
+class ContextMenu:
+    BUTTON_HEIGHT = 20
+    BUTTON_WIDTH = 50
+
+    def __init__(self, window: pyglet.window.Window, x: int, y: int):
+        self.frame = pyglet.gui.Frame(window)
+        self.x = x
+        self.y = y
+        self.button_amount = 0
+        self.batch = pyglet.graphics.Batch()
+
+    def add_button(self, pressed, depressed, hover=None, handler=None):
+        self.button_amount += 1
+        button = pyglet.gui.PushButton(
+            self.x,
+            self.y-self.BUTTON_HEIGHT*self.button_amount,
+            pyglet.resource.image(pressed),
+            pyglet.resource.image(depressed),
+            hover and pyglet.resource.image(hover),
+            self.batch
+        )
+        button.set_handler("on_press", handler)
+        self.frame.add_widget(button)
+
+    def point_intersects(self, x, y) -> bool:
+        return (
+                self.x < x < self.x + self.BUTTON_WIDTH
+                and self.y - self.BUTTON_HEIGHT*self.button_amount < y < self.y
+        )
+
+
 class GameOfLifeWindow(pyglet.window.Window):
     """Window managing the game of life."""
 
@@ -15,11 +46,14 @@ class GameOfLifeWindow(pyglet.window.Window):
             width = WIDTH
         super().__init__(width, height, *args, **kwargs)
         self.grid = GridManager(start_grid, height, width)
+        self.context_menu = None
 
     def on_draw(self) -> None:
         """Clear window and draw grid's batch."""
         self.clear()
         self.grid.batcher.draw()
+        if self.context_menu is not None:
+            self.context_menu.batch.draw()
 
     def on_key_press(self, symbol: int, modifiers: int) -> None:
         """Run a single generation when a key is pressed."""
@@ -27,4 +61,14 @@ class GameOfLifeWindow(pyglet.window.Window):
 
     def on_mouse_press(self, x: int, y: int, button: int, modifiers: int) -> None:
         """Switch cell state on mouse clicks."""
-        self.grid.switch_cell_at(x // CELL_SIZE, y // CELL_SIZE)
+        if button == pyglet.window.mouse.RIGHT:
+            self.context_menu = ContextMenu(self, x, y)
+            self.context_menu.add_button(
+                "placeholder.png",
+                "placeholder.png", handler=lambda: pyglet.clock.unschedule(self.grid.run_generation)
+            )
+            self.context_menu.add_button("placeholder.png", "placeholder.png")
+        elif button == pyglet.window.mouse.LEFT:
+            if not self.context_menu.point_intersects(x, y):
+                self.grid.switch_cell_at(x // CELL_SIZE, y // CELL_SIZE)
+            self.context_menu = None
