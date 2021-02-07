600c59cbf3629499dcf6098275a2facbb6b56fe0
Numerlor <25886452+Numerlor@users.noreply.github.com>
2021-02-07 17:02:47 +0100

    Abstract the grid into its own class

--- game_of_life/__init__.py
+++ game_of_life/__init__.py
@@ -1,7 +1,7 @@
 from .constants import *
 from .cell import Cell
-from .grid import GridManager
+from .grid import GameOfLife
 from .window import GameOfLifeWindow
 
-__all__ = ["Cell", "GridManager", "GameOfLifeWindow", "CELL_SIZE", "HEIGHT", "WIDTH", "SIMULATION_TICK"]
+__all__ = ["Cell", "GameOfLife", "GameOfLifeWindow", "CELL_SIZE", "HEIGHT", "WIDTH", "SIMULATION_TICK"]
 
