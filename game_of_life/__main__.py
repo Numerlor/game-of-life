df8ef1b2ef9396a46a07980226cd40f5b4a16a04
Numerlor <25886452+Numerlor@users.noreply.github.com>
2021-02-08 21:47:33 +0100

    Pass path in proper form to load_grid_from_file

--- game_of_life/__main__.py
+++ game_of_life/__main__.py
@@ -1,4 +1,5 @@
 import sys
+from pathlib import Path
 
 import pyglet
 
@@ -7,7 +8,7 @@ from .utils import load_grid_from_file, pad_grid
 
 start_grid = None
 if len(sys.argv) > 1:
-    start_grid = load_grid_from_file(sys.argv[1])
+    start_grid = load_grid_from_file(Path(sys.argv[1]))
     pad_grid(start_grid, 1)
 window = GameOfLifeWindow(start_grid)
 pyglet.app.run()
