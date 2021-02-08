cc778ba1075f69a047dedffc98577e5e7fc406ab
Numerlor <25886452+Numerlor@users.noreply.github.com>
2021-02-08 21:50:17 +0100

    Add image resources

--- game_of_life/__main__.py
+++ game_of_life/__main__.py
@@ -6,6 +6,9 @@ import pyglet
 from game_of_life import GameOfLifeWindow
 from .utils import load_grid_from_file, pad_grid
 
+pyglet.resource.path.append("../resources")
+pyglet.resource.reindex()
+
 start_grid = None
 if len(sys.argv) > 1:
     start_grid = load_grid_from_file(Path(sys.argv[1]))
